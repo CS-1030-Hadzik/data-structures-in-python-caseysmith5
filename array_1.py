ecd0304bcb0610789cea6305fbf95206e3b977b7
caseysmith5 <114037123+caseysmith5@users.noreply.github.com>
2022-12-07 15:13:47 -0700

    code

--- array_1.py
+++ array_1.py
@@ -1,12 +1,15 @@
 print("Casey Smith")
 cars = ['Ford', 'Chrysler', 'Dodge','Ram', 'Jeep', 'Chevy', 'GMC']
 print(cars)
-print = len(cars)
-print = cars[3]
+print(len(cars))
+print(cars[3])
 cars.insert(2, "Toyota")
 print(cars)
 cars.pop(5)
 print(cars)
-cars.reverse()
+print(cars.reverse())
 
+my_array_length = len(cars)
+array_length = 'The length of my array is'
 
+print(array_length, str(my_array_length))
