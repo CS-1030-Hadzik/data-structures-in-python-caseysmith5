e932a647c27b26705b78251dba4ffde598fce66a
caseysmith5 <114037123+caseysmith5@users.noreply.github.com>
2022-12-06 21:29:53 -0700

    Line Print

--- array_1.py
+++ array_1.py
@@ -1,10 +1,10 @@
 print("Casey Smith")
-
-
 cars = ["Ford", "Chrysler", "Dodge","Ram", "Jeep", "Chevy", "GMC"]
 print(cars)
 print = len(cars)
 print = cars[3]
 cars.insert(2, "Toyota")
+print(cars)
 cars.pop(5)
+print(cars)
 
